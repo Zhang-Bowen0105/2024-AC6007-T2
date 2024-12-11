9533e0bca1c40c5ad0f014936b55c1e147cad4ab
Zhang-Bowen0105 <evanzbw0105@gmail.com>
2024-12-11 04:13:42 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template,request
+import textblob
 
 app = Flask(__name__)
 
@@ -7,5 +8,16 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/SA",methods=["Get","POST"])
+def  SA():
+    return(render_template("SA.html"))
+    
+@app.route("/SA_result",methods=["Get","POST"])
+def  SA_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return(render_template("SA_result.html", r=r))
+
 if __name__ == "__main__":
     app.run()
+
